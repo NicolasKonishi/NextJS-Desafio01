ddd2d831cc0641477ad9e77aebfadf91ea061e02
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-10-23 23:35:31 -0300

    finishi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-export default App
--- src/pages/categoria.js
+++ src/pages/categoria.js
@@ -1,6 +1,5 @@
 
 export default function Categoria() {
-
     return(
         <div>
             <h1>Categoria</h1>
